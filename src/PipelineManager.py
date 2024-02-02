453b8a7b32741a48088d85549a70ddcb56b81f1e
bitwiserob <kaczurr@gmail.com>
2024-02-02 17:14:00 -0500

    added blank pipeline manager

---
+++ src/PipelineManager.py
@@ -0,0 +1,8 @@
+class PipelineManager:
+    def __init__(self):
+        self.models = {}
+        # TODO: Add other components like models and model runner
+
+    def run_pipeline(self, data):
+        # TODO: Implement the pipeline running logic
+        pass
